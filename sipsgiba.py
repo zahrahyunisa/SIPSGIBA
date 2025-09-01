040a5aa11cf28d69e432c7af06be7c07506c9e8d
zahrahyunisa <zahrahyunisa@gmail.com>
2025-09-02 02:23:43 +0700

    Update sipsgiba.py

--- sipsgiba.py
+++ sipsgiba.py
@@ -400,7 +400,7 @@ else:
                 st.subheader("Visualisasi Cluster 1 (C1)")
                 df_c1_filtered = df_pca_filtered[df_pca_filtered['Cluster'] == '1']
                 fig_c1 = px.scatter(
-                    df_pca_filtered, x='Dim1', y='Dim2',
+                    df_c1_filtered, x='Dim1', y='Dim2',
                     hover_name='Nama_Posyandu', title='Cluster 1 (C1) PCA Scatter Plot',
                     color_discrete_sequence=['#43AA8B']
                 )
@@ -416,7 +416,7 @@ else:
                 st.subheader("Visualisasi Cluster 2 (C2)")
                 df_c2_filtered = df_pca_filtered[df_pca_filtered['Cluster'] == '2']
                 fig_c2 = px.scatter(
-                    df_pca_filtered, x='Dim1', y='Dim2',
+                    df_c2_filtered, x='Dim1', y='Dim2',
                     hover_name='Nama_Posyandu', title='Cluster 2 (C2) PCA Scatter Plot',
                     color_discrete_sequence=['#F9C74F']
                 )
